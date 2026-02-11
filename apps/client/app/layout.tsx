27926f4c9e6cdb5cd92ff3aa000690f3eaed4839
kimjumong <rlawnahd123@naver.com>
2026-02-11 11:01:21 +0900

    feat: Google Tag Manager 스크립트 추가 - GTM 스크립트 추가 - noscript 태그로 iframe 추가

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -33,12 +33,29 @@ export default function RootLayout({
         <html lang="ko" suppressHydrationWarning>
             <head>
                 <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WLVG6WBJ');`,
+                    }}
+                />
+                <script
                     async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8213713469516212"
                     crossOrigin="anonymous"
                 />
             </head>
             <body className="antialiased">
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-WLVG6WBJ"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <ThemeProvider>
                     <QueryProvider>
                         <Header />
